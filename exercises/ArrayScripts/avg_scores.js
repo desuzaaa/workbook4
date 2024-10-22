a44841904a5b2dd19930c6478517ce7efdd06ab3
Kameron de Suza <z6nekam@gmail.com>
2024-10-22 10:57:15 -0400

    didnt fully complete foods.js

--- exercises/ArrayScripts/avg_scores.js
+++ exercises/ArrayScripts/avg_scores.js
@@ -1,13 +1,13 @@
-let MyScores = [92, 98, 84, 76, 89, 99, 100];
+let myScores = [92, 98, 84, 76, 89, 99, 100];
 
 let yourScores = [82, 98, 94, 88, 92, 100, 100];
 
 function getAverage(scores) {
     let average = 0
     for (let index = 0; index < scores.length; index++) {
-        const element = scores[index];
+       //  const element = scores[index];
 
-        console.log(scores[index]);
+        // console.log(scores[index]);
         average = (average + scores [index])
 
     }
@@ -15,4 +15,4 @@ function getAverage(scores) {
     
 }
 
-getAverage(MyScores)
+getAverage(myScores)
