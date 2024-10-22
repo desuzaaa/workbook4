6d8bc328ae637fa3535326ea6ae4ee77b6f1deb6
Kameron de Suza <z6nekam@gmail.com>
2024-10-22 10:15:29 -0400

    did average and family exercises

---
+++ exercises/ArrayScripts/my_family.js
@@ -0,0 +1,9 @@
+let contacts = [{ name: "John" }, { name: "Sam" }, { name: "Kyle" }, { name: "Laura" }];
+
+
+for (let index = 0; index < contacts.length; index++) {
+    const item = contacts[index];
+    
+    console.log(item.name);
+    
+}
